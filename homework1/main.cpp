7502528c549df79056cceae57de74bb5958c4889
Edgar <edgar.rakhmankulov@yandex.ru>
2021-02-22 02:51:56 +0300

    triangle checked

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 int main()
 {
     Point *a = new Point[3]{Point(0, 0), Point(0, 3), Point(4, 0)};
-    ClosedPolygonalChain b(3, a);
-    ClosedPolygonalChain p(b);
+    ClosedPolygonalChain c(3, a);
+    ClosedPolygonalChain p(c);
+    Triangle t(3, a);
+
     assert(equalsf(p.perimeter(), 12));
+    assert(t.hasRightAngle());
 }
