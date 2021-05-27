be97923d2551dc06961ccda34e452d980a3f76ab
Edgar <edgar.rakhmankulov@yandex.ru>
2021-05-28 01:49:11 +0300

    changeCapacity

--- homework5/CircularBuffer.hpp
+++ homework5/CircularBuffer.hpp
@@ -32,20 +32,59 @@ class CircularBuffer
 		}
 		else
 		{
-			first_ = index(ptr + i);
-			data_[first_] = t;
+			ptr = index(ptr + i);
+			data_[ptr] = t;
 			increaseItemAmount();
 		}
+		std::cout << first_ << " " << last_ << " " << ptr << std::endl;
 	}
 
 	void addFirst(T t)
 	{
-		addItem(t, -1);
+		//addItem(t, -1);
+		if (itemsAmount_ == 0)
+		{
+			data_[first_] = t;
+			increaseItemAmount();
+		}
+		else
+		{
+			first_ = index(first_ - 1);
+			data_[first_] = t;
+			increaseItemAmount();
+		}
+		std::cout << first_ << " " << last_ << std::endl;
 	}
 
 	void addLast(T t)
 	{
-		addItem(t, 1);
+		//addItem(t, 1);
+		if (itemsAmount_ == 0)
+		{
+			data_[last_] = t;
+			increaseItemAmount();
+		}
+		else
+		{
+			last_ = index(last_ + 1);
+			data_[last_] = t;
+			increaseItemAmount();
+		}
+		std::cout << first_ << " " << last_ << std::endl;
+	}
+
+	void delFirst()
+	{
+		data_[first_] = 0;
+		first_ = index(first_ + 1);
+		decreaseItemAmount();
+	}
+
+	void delLast()
+	{
+		data_[last_] = 0;
+		last_ = index(last_ - 1);
+		decreaseItemAmount();
 	}
 
 	T &first() const
@@ -75,5 +114,27 @@ class CircularBuffer
 	{
 		if (itemsAmount_ > 0)
 			itemsAmount_--;
+	}
+
+	void changeCapacity(int newCapacity)
+	{
+		int itemsFit = (itemsAmount_ < newCapacity) ? itemsAmount_ : newCapacity;
+		T *buf = new T[newCapacity];
+		for (int i = 0; i < itemsFit; i++)
+		{
+			buf[i] = data_[index(first_ + i)];
+		}
+		itemsAmount_ = itemsFit;
+		data_ = buf;
+		first_ = 0;
+		last_ = itemsAmount_ - 1;
+		capacity_ = newCapacity;
+	}
+
+	void show() const
+	{
+		for (int i = 0; i < capacity_; i++)
+			std::cout << data_[i] << " ";
+		std::cout << std::endl;
 	}
 };
