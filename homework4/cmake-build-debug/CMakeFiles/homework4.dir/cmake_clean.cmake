0d65430cd97c5ddd8b2f1b7dccbaf97c6f7257b5
edgar r <oakmillennial@gmail.com>
2021-09-01 01:06:12 +0300

    passed

---
+++ homework4/cmake-build-debug/CMakeFiles/homework4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework4.dir/main.cpp.obj"
+  "homework4.exe"
+  "homework4.exe.manifest"
+  "homework4.pdb"
+  "libhomework4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework6/cmake-build-debug/CMakeFiles/homework6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework6.dir/main.cpp.obj"
+  "homework6.exe"
+  "homework6.exe.manifest"
+  "homework6.pdb"
+  "libhomework6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
